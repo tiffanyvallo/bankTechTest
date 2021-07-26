b0968abaad23100a8ef63774319d6a6055dad7a5
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-26 16:51:46 +0100

    FAILING TEST updated opening balance not a function but part of contructor

--- test/account.test.js
+++ test/account.test.js
@@ -2,5 +2,5 @@ const Account = require('../src/account');
 
 test('can create an account with an opening balance of 0', () => {
   let account = new Account
-  expect(account.openingBalance(openingBalance)).toBe(0)
+  expect(account.openingBalance).toBe(0)
 })
