cd4e4c597be46f6e3114d5b56faf04587cee4666
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-26 19:55:07 +0100

    FAILING TEST: transaction class still failing, test added to check for attirbutes of constructor

--- test/transaction.test.js
+++ test/transaction.test.js
@@ -1,2 +1,8 @@
 const Transaction = require('../src/transaction');
 
+test('Transaction Class has attributes', () => {
+  transaction = new Transaction(10000, 10000)
+  expect(transaction.date).toEqual(new Date().toLocaleDateString())
+  expect(transaction.amount).toEqual(10000)
+  expect(transaction.currentBalance).toEqual(10000)
+})
