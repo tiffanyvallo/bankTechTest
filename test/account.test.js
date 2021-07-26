6f27e7174c691be5cb1716f77715304efff3367a
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-26 17:28:45 +0100

    FAILING TEST: withdraw method testing

--- test/account.test.js
+++ test/account.test.js
@@ -9,7 +9,6 @@ test('can create an account with an opening balance of 0', () => {
 })
 
 describe('#deposit', () => {
-
   test('can save a deposit into an array of transactions', () => {
     account.deposit(10000)
     expect(account.transactionHistoy).toContain(10000)
@@ -18,5 +17,12 @@ describe('#deposit', () => {
   test('can deposit money into an account', () => {
     account.deposit(10000)
     expect(account.balance()).toEqual(10000)
+  })
+})
+
+describe('#withdraw', () => {
+  test('can save a deposit into an array of transactions', () => {
+    account.withdraw(7000)
+    expect(account.transactionHistoy).toContain(-7000)
   })
 })
