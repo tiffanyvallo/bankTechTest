482cfba8997273b23359b7975778f877dc8f9ece
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-26 17:14:18 +0100

    FAILING TEST deposit method in account class

--- test/account.test.js
+++ test/account.test.js
@@ -1,6 +1,16 @@
 const Account = require('../src/account');
 
+beforeEach(function() {
+  account = new Account()
+})
+
 test('can create an account with an opening balance of 0', () => {
-  let account = new Account
   expect(account.openingBalance).toBe(0)
+})
+
+describe('#deposit', () => {
+  test('can deposit money into an account', () => {
+    account.deposit(10000)
+    expect(account.balance()).toEqual(10000)
+  })
 })
