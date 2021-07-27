50eae09d134c4c0f89fd26bed88df9b87a556a6e
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-27 10:30:02 +0100

    refactored code added error checks into private methods in account class

--- src/account.js
+++ src/account.js
@@ -7,28 +7,36 @@ class Account {
   }
 
   deposit = (amount) => {
-    if (isNaN(amount) === true) {
-        throw new Error('Invalid Data Type: Please enter an integer')
-    } else if (amount < 0) {
-        throw new Error('Invalid Amount: Cannot deposit amount less than £0')
-    }
+    this._depositChecks(amount)
     return this.transactionHistoy.push(new Transaction(amount, amount + this.balance()))
   }
 
   withdraw = (amount) => {
-    if (isNaN(amount) === true) {
-        throw new Error('Invalid Data Type: Please enter an integer')
-    } else if (amount < 0) {
-        throw new Error('Invalid Amount: Cannot withdraw amount less than £0')
-    } else if (amount > this.balance() + this.overdraftLimit) {
-        throw new Error('Insufficient Funds: Cannot withdraw money')
-    }
+    this._withdrawChecks(amount)
     let withdrawal = -amount
     return this.transactionHistoy.push(new Transaction(withdrawal, this.balance() - amount))
   }
 
   balance = () => {
     return this.transactionHistoy.map(transaction => transaction.amount).reduce((a, b) => a + b, 0)
+  }
+
+  _depositChecks = (amount) => {
+    if (isNaN(amount) === true) {
+        throw new Error('Invalid Data Type: Please enter an integer')
+  } else if (amount < 0) {
+        throw new Error('Invalid Amount: Cannot deposit amount less than £0')
+  }
+  }
+
+  _withdrawChecks = (amount) => {
+    if (isNaN(amount) === true) {
+      throw new Error('Invalid Data Type: Please enter an integer')
+  } else if (amount < 0) {
+      throw new Error('Invalid Amount: Cannot withdraw amount less than £0')
+  } else if (amount > this.balance() + this.overdraftLimit) {
+      throw new Error('Insufficient Funds: Cannot withdraw money')
+  }
   }
 }
 
