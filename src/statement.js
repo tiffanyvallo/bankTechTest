f1b5d83f5f9239c270777a163d2f6812da8c7a60
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-28 12:02:16 +0100

    PASSING TEST: statement class can correctly format transactions

--- src/statement.js
+++ src/statement.js
@@ -1,5 +1,33 @@
 class Statement{
 
+  header = () => {
+    return 'date    ||  credit  ||  debit  || balance\n'
+  }
+
+  formatTransaction = (transaction) => {
+    if (transaction.amount < 0) {
+      return this._debitTransaction(transaction)
+    } else {
+      return this._creditTransaction(transaction)
+    }
+  }
+
+  formatStatement = (transactionHistory) => {
+    return transactionHistory.map((transaction) => this.formatTransaction(transaction))
+  }
+
+  printStatement = (transactionHistory) => {
+    return this.header() + this.formatStatement(transactionHistory).reverse().join('\n')
+  }
+
+  _debitTransaction = (transaction) => {
+    let formattedAmount = Math.abs(transaction.amount)
+    return `${transaction.date} ||  || ${formattedAmount.toFixed(2)} || ${transaction.currentBalance.toFixed(2)}`
+  }
+
+  _creditTransaction = (transaction) => {
+    return `${transaction.date} || ${transaction.amount.toFixed(2)} ||  || ${transaction.currentBalance.toFixed(2)}`
+  }
 }
 
 module.exports = Statement
--- test/statement.test.js
+++ test/statement.test.js
@@ -7,5 +7,5 @@ test('can print a statement in the correct format', () => {
   let mockTransaction3 = {date: '14/01/2012', amount: -500, currentBalance: 2500}
   const mockTransactionHistory = [mockTransaction1, mockTransaction2, mockTransaction3]
   
-  expect(statement.printStatement(mockTransactionHistory)).toEqual('|    date    ||  credit  ||  debit  || balance  |\n| 14/01/2012 ||  || 500.00 || 2500.00 |\n| 13/01/2012 || 2000.00 ||  || 3000.00 |\n| 10/01/2012 || 1000.00 ||  || 1000.00 |')
+  expect(statement.printStatement(mockTransactionHistory)).toEqual('date    ||  credit  ||  debit  || balance\n14/01/2012 ||  || 500.00 || 2500.00\n13/01/2012 || 2000.00 ||  || 3000.00\n10/01/2012 || 1000.00 ||  || 1000.00')
 })
