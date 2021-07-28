e84d005fe1acaeacbe94f658a7764ba06c822c47
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-28 15:23:17 +0100

    PASSING TEST: Feature test added and passing

---
+++ test/feature.test.js
@@ -0,0 +1,10 @@
+const Account = require('../src/account')
+
+test('can meet the specifications and print the correct results on a array', () => {
+  let account = new Account
+  const statmentSpy = jest.spyOn(console, 'log')
+  account.deposit(909298.88)
+  account.withdraw(4563.77)
+  account.viewStatement()
+  expect(statmentSpy).toHaveBeenCalledWith(`date    ||  credit  ||  debit  || balance\n${new Date().toLocaleDateString()} ||  || 4563.77 || 904735.11\n${new Date().toLocaleDateString()} || 909298.88 ||  || 909298.88`)
+})
