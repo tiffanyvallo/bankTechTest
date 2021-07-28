923749d5d1f47c88b0f591d06162aefd18c29367
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-28 11:49:35 +0100

    FAILING TEST: Statement class can print the correct statement format

--- test/statement.test.js
+++ test/statement.test.js
@@ -1,1 +1,11 @@
 const Statement = require('../src/statement')
+
+test('can print a statement in the correct format', () => {
+  let statement = new Statement
+  let mockTransaction1 = {date: '10/01/2012', amount: 1000, currentBalance: 1000}
+  let mockTransaction2 = {date: '13/01/2012', amount: 2000, currentBalance: 3000}
+  let mockTransaction3 = {date: '14/01/2012', amount: -500, currentBalance: 2500}
+  const mockTransactionHistory = [mockTransaction1, mockTransaction2, mockTransaction3]
+  
+  expect(statement.printStatement(mockTransactionHistory)).toEqual('|    date    ||  credit  ||  debit  || balance  |\n| 14/01/2012 ||  || 500.00 || 2500.00 |\n| 13/01/2012 || 2000.00 ||  || 3000.00 |\n| 10/01/2012 || 1000.00 ||  || 1000.00 |')
+})
